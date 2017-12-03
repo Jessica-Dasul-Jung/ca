4c6c1213434dfaf73c88720e76d4d9b6a0471d03
Jessica Dasul Jung <sicajung0214@gmail.com>
2017-12-02 23:09:02 -0800

    Fixed schema A/B checkstate problem

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -59,74 +59,12 @@ void MainWindow::calculate()
 
 void MainWindow::useSchemaA()
 {
-    if (ui->schemaB->checkState() == Qt::Checked)
-        ui->schemaB->setCheckState (Qt::Unchecked);
-
+    ui->schemaB->setCheckState(Qt::Unchecked);
     m_calc->setSchema(SCHEMA_A);
 }
 
 void MainWindow::useSchemaB()
 {
-    if (ui->schemaA->checkState() == Qt::Checked)
-        ui->schemaA->setCheckState (Qt::Unchecked);
-
+    ui->schemaA->setCheckState(Qt::Unchecked);
     m_calc->setSchema(SCHEMA_B);
-}
-
-void MainWindow::display()
-{
-    /*
-    hwl[0] = new QLabel ("HW1", this);
-    hwl[1] = new QLabel ("HW2", this);
-    hwl[2] = new QLabel ("HW3", this);
-    hwl[3] = new QLabel ("HW4", this);
-    hwl[4] = new QLabel ("HW5", this);
-    hwl[5] = new QLabel ("HW6", this);
-    hwl[6] = new QLabel ("HW7", this);
-    hwl[7] = new QLabel ("HW8", this);
-
-    for (int i = 0; i < HW_NUM; i++)
-    {
-        hwl[i]->setGeometry (QRect(QPoint(50, 50), QSize(50, 200)));
-    }
-
-    coursel = new QLabel ("Course:", this);
-    midterml[0] = new QLabel ("Midterm 1", this);
-    midterml[1] = new QLabel ("Midterm 2", this);
-    finall = new QLabel ("Final Exam", this);
-    schemal[0] = new QLabel ("Schema A", this);
-    schemal[1] = new QLabel ("Schema B", this);
-
-    scorel = new QLabel ("Overall Score: ", this);
-
-    QWidget* score = new QWidget (this);
-    score->setObjectName("0.0");
-
-
-    //QVBoxLayout* vbox = new QVBoxLayout (this);
-
-    hws[0] = new QSlider (Qt::Horizontal);
-    //hws[0]->setFocusPolicy (Qt::StrongFocus);
-    hws[0]->setTickPosition (QSlider::TicksBelow);
-    hws[0]->setTickInterval(10);
-    //hws[0]->setSingleStep(1);
-    hws[0]->setMinimum(0);
-    hws[0]->setMaximum(100);
-    //hws[0]->setGeometry(QRect(QPoint(50, 50), QSize(200, 10)));
-
-
-    //vbox->addWidget(hws[0]);
-    hwb[0] = new QScrollBar (Qt::Horizontal);
-    hwb[0]->setMinimum(0);
-    hwb[0]->setMaximum(100);
-
-    auto layout = new QGridLayout(this);
-    layout->addWidget(hws[0]);
-    layout->addWidget(hwb[0]);
- //   hwb[0]->setFocusPolicy (Qt::StrongFocus);
-   // hwb[0]->setGeometry(QRect(QPoint(100, 100), QSize(50, 200)));
-
-   // connect (hws[0], SIGNAL(valueChanged(int)), hwb[0], SLOT(setValue(int)));
-
-    */
 }
